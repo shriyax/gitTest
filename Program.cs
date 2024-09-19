7f7270d58a47179815920704ce5cdf7cfd890d36
shriya <20wh1a05b1@bvrithyderababd.edu.in>
2024-09-19 14:35:33 +0530

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
-﻿MyMethod();
+﻿void OneMethod()
+{
+    Console.WriteLine(" I wrote a method ");
+}
 
-void MyMethod()
+void TwoMethod()
+{
+    Console.WriteLine(" I wrote a method ");
+}
+
+void ThreeMethod()
 {
     Console.WriteLine(" I wrote a method ");
 }
