48dbebb325139110ebd9dfc9a903e918ad157ebb
shriya <20wh1a05b1@bvrithyderababd.edu.in>
2024-09-19 12:05:06 +0530

    multiple entries

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, github workflow. This is a documentation");
+Console.WriteLine("Hello, github workflow. This is a documentation");
+Console.WriteLine("Hello, github workflow. This is a documentation");
+Console.WriteLine("Hello, github workflow. This is a documentation");
+Console.WriteLine("Hello, github workflow. This is a documentation");
+Console.WriteLine("Hello, github workflow. This is a documentation");
